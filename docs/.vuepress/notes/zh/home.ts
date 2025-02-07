3eba8af25aad64cd3c2dfe00bd57bdf3da0dfa9b
赵亦凡 <1784848264@qq.com>
2025-02-07 17:25:37 +0800

    vuepress@3.0

--- docs/.vuepress/notes/zh/home.ts
+++ docs/.vuepress/notes/zh/home.ts
@@ -6,5 +6,10 @@ export const home = defineNoteConfig({
     // 声明笔记的链接前缀，与 `notes.link` 拼接，这里表示 `/homes/`
     // 笔记内的所有文章会以 `/homes/` 作为访问链接前缀。
     link: '/homes/',
-    sidebar: 'auto',
+    sidebar: [
+        { text: '快速上手', link: '/homes/installation/',icon: 'logos:vue' },
+        { text: '全局配置', link: '/homes/global/',icon: 'icon-park:setting' },
+        { text: '全局API', link: '/homes/api/',icon: 'tabler:api' },
+        { text: '国际化', link: '/homes/locale/',icon: 'ix:language-filled' },
+    ],
 })
